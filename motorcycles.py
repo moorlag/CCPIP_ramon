475d9063c4ac21dd9711faa920cb7fae18f6147b
Ramon Moorlag <r.moorlag@gmail.com>
2019-07-25 21:30:07 +0300

    Update Motorcycles.py

--- motorcycles.py
+++ motorcycles.py
@@ -1,5 +1,7 @@
-motorcycles = ['honda', 'yamaha', 'suziku']
-print (motorcycles)
+motorcycles = ['honda', 'yamaha', 'suziku', 'ducati']
+print(motorcycles)
 
-del motorcycles[0]
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
 print (motorcycles)
+print ("/nA " + too_expensive.title() + " is de duur voor mij")
