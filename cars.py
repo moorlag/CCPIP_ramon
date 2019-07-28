a3e24e573177d86a1b75d7f3c8854f8be8d7a445
Ramon Moorlag <r.moorlag@gmail.com>
2019-07-28 20:13:53 +0300

    cars.py
    lol met lijsten :-)

---
+++ cars.py
@@ -0,0 +1,14 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print('De orginele lijst')
+print(cars)
+
+print('Hier is de lijst met organisatie: ')
+print (sorted(cars))
+
+print('De orginele lijst')
+print(cars)
+
+cars.reverse()
+print(cars)
+
+len(cars)
