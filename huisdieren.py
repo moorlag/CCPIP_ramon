b18ee6b1718aa2ba79b307168325a6711e704278
Ramon Moorlag <r.moorlag@gmail.com>
2019-07-28 22:58:33 +0300

    huisdieren.py
    jaja, een eenhoorn is ook een beest met gevoelens.

---
+++ huisdieren.py
@@ -0,0 +1,4 @@
+huisdier = ['hond', 'cat', 'cavia', 'marmot', 'varaan', 'eenhoorn']
+for dier in huisdier:
+    print (dier.title() + ", ook zo'n lief feest")
+print ("I hartje beesten")
---
+++ pizza.py
@@ -0,0 +1,4 @@
+pizza = ['hawaii', 'niet_hawaii', 'vega', 'salami', 'kaas', 'alfa alfa']
+for stukken in pizza:
+    print (stukken.title() + ", ook een lekkere pizza!")
+print ("I hartje pizza")
