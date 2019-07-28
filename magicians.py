6ce39dae03a801bf962f1ae38090f2d34095bbd9
Ramon Moorlag <r.moorlag@gmail.com>
2019-07-28 22:29:10 +0300

    Create magicians.py
    fun met loops, LUN?

---
+++ magicians.py
@@ -0,0 +1,5 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print (magician.title() + ", dat was nog eens een grote truuk!")
+    print ('Ik kan niet wachten om je volgende truuk te zien ' +magician.title() + ".\n")
+print("Bedankt voor je inspanningen en tot de volgende keer!")
