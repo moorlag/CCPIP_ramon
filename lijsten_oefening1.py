97aee43949c24faa41816805f76e3141ef784d92
Ramon Moorlag <r.moorlag@gmail.com>
2019-07-29 21:28:47 +0300

    squares

---
+++ lijsten_oefening1.py
@@ -0,0 +1,3 @@
+for val in range (1,1000001):
+
+print('Maximum is:', max(val))
--- uitwerkingen/nummers.py
+++ uitwerkingen/nummers.py
@@ -1,2 +1,4 @@
 for value in range (1,5):
     print (value)
+even_numbers = list (range (2,11,2))
+print (even_numbers)
