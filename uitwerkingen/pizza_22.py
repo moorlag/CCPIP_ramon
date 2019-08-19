bd1703702fb017b2e22095c99f8b27eea3ae4282
Ramon Moorlag <r.moorlag@gmail.com>
2019-08-19 14:03:50 +0200

    Laatste oefening hoofdstuk 6 :+1:

---
+++ uitwerkingen/pizza_22.py
@@ -0,0 +1,9 @@
+pizza = {
+'crust' : 'thick',
+'toppings' : ['musroom', 'extra cheese'],
+}
+
+print ("You ordered a " + pizza['crust'] + "-crust pizza" + "with the folling toppings:")
+
+for toppings in pizza ['toppings']:
+    print (toppings)
---
+++ uitwerkingen/woordenlijst_h6_3.py
@@ -0,0 +1,9 @@
+fav_languages = {
+"Lijst": 'Een slimme verzameling, zonder verband',
+"Woordenboek": 'Ook een slimme lijst maar met een index',
+"Int": "Een geheel getal",
+"Float": "Getal met een komma",
+}
+for name in fav_languages.items():
+    print ("Deze termen heb ik voorbij zien komen ")
+    print (name)
